d5a46ea33692fbc5aea30c0f0d0d865e700617e1
long9657 <longlehien9657@gmail.com>
2025-02-21 00:17:01 +0700

    new config

--- Terminal/Microsoft.PowerShell_profile.ps1
+++ Terminal/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,13 @@
-oh-my-posh init pwsh --config 'C:\Users\long\AppData\Local\Programs\oh-my-posh\themes\dracula.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyo.omp.json" | Invoke-Expression
+function goCode { 
+  cd D:\Code\Competitive_Code
+}
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath ([System.IO.Path]::GetFullPath($cwd))
+    }
+    Remove-Item -Path $tmp
+}
--- Vim/readme.md
+++
@@ -1,1 +0,0 @@
-Place file in C:/Users/long/
